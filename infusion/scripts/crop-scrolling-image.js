f9c82294c4f422509345792981de9a50d2a38427
InsightfulAI <insightfulai@gmail.com>
2020-01-12 21:40:17 +1000

    Fixed a few bugs
    - Fixed jitter bug
    - Now redirects without the slash

--- infusion/scripts/crop-scrolling-image.js
+++ infusion/scripts/crop-scrolling-image.js
@@ -2,25 +2,25 @@ function lerp(v0, v1, t) {
     return v0*(1-t)+v1*t
 }
 
-function cropImage(imageClass) {
-    var windowHeight = jQuery( window ).height();
-
+function centerBackground(imageClass) {
+    var ratio = 1080/1620;
+    var windowHeight = $( window ).height();
+    var iamgeWidth = windowHeight * ratio;
     $(imageClass).each(function(){
-        var thisPos = $(this).offset().top;
-        var topOfWindow = $(window).scrollTop();
-        if (topOfWindow + windowHeight > thisPos ) {
-            distance = Math.min(topOfWindow-thisPos, 0)
-            this.setAttribute("style", "object-position: 50% "+ distance + "px;");
+        var thisWidth = $(this).width();
+        if (thisWidth < iamgeWidth) {
+            var widthDiff = iamgeWidth - thisWidth;
+            this.style.backgroundPosition = -(widthDiff/2) + "px";
         }
     })
 }
 
+
 // if the image in the window of browser when the page is loaded, update images
 $(document).ready(function(){
-    cropImage(".scrolling-target")
+    centerBackground(".parallax")
 });
 
-// if the image in the window of browser when scrolling the page, update images
-$(window).scroll(function() {
-    cropImage(".scrolling-target")
+$(window).resize(function(){
+    centerBackground(".parallax")
 });
